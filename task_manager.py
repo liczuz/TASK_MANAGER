66d434667869c2e5dc58179ce1590cd3f66d11c0
liczuz <zuzana.lichtenbergova@t-mobile.cz>
2025-07-17 07:18:14 +0200

    Add files via upload

--- task_manager.py
+++ task_manager.py
@@ -5,7 +5,7 @@ def pripojeni_db():
     try:
         spojeni = mysql.connector.connect(
             host="localhost",
-            user="root",           
+            user="root",
             password="K4d1budk487!",
             database="TASK_MANAGER"
         )
@@ -21,7 +21,7 @@ def vytvoreni_tabulky():
         kurzor.execute("""
             CREATE TABLE IF NOT EXISTS ukoly (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                nazev VARCHAR(255) NOT NULL,
+                nazev VARCHAR(100) NOT NULL,
                 popis TEXT NOT NULL,
                 stav ENUM('nezahájeno', 'probíhá', 'hotovo') DEFAULT 'nezahájeno',
                 datum_vytvoreni DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -31,7 +31,110 @@ def vytvoreni_tabulky():
         kurzor.close()
         spojeni.close()
 
-def hlavni_menu():
+def pridat_ukol(nazev, popis):
+    if not nazev or not popis:
+        return False
+    spojeni = pripojeni_db()
+    if spojeni:
+        kurzor = spojeni.cursor()
+        dotaz = "INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)"
+        kurzor.execute(dotaz, (nazev, popis))
+        spojeni.commit()
+        kurzor.close()
+        spojeni.close()
+        return True
+    return False
+
+def zobrazit_ukoly():
+    spojeni = pripojeni_db()
+    if spojeni:
+        kurzor = spojeni.cursor(dictionary=True)
+        kurzor.execute("SELECT * FROM ukoly WHERE stav IN ('nezahájeno', 'probíhá')")
+        vysledky = kurzor.fetchall()
+        kurzor.close()
+        spojeni.close()
+        return vysledky
+    return []
+
+def existuje_aktivni_ukol(id_ukolu):
+    spojeni = pripojeni_db()
+    if spojeni:
+        kurzor = spojeni.cursor()
+        kurzor.execute(
+            "SELECT COUNT(*) FROM ukoly WHERE id = %s AND stav IN ('nezahájeno', 'probíhá')",
+            (id_ukolu,))
+        vysledek = kurzor.fetchone()
+        kurzor.close()
+        spojeni.close()
+        return vysledek and vysledek[0] > 0
+    return False
+
+def aktualizovat_ukol(id_ukolu, novy_stav):
+    if novy_stav not in ['probíhá', 'hotovo']:
+        return "neplatny_stav"
+
+    if not existuje_aktivni_ukol(id_ukolu):
+        return "neexistuje"
+
+    spojeni = pripojeni_db()
+    if spojeni:
+        kurzor = spojeni.cursor()
+        kurzor.execute("UPDATE ukoly SET stav = %s WHERE id = %s", (novy_stav, id_ukolu))
+        spojeni.commit()
+        kurzor.close()
+        spojeni.close()
+        return True
+    return False
+
+def odstranit_ukol(id_ukolu):
+    if not existuje_aktivni_ukol(id_ukolu):
+        return False
+
+    spojeni = pripojeni_db()
+    if spojeni:
+        kurzor = spojeni.cursor()
+        dotaz = "DELETE FROM ukoly WHERE id = %s"
+        kurzor.execute(dotaz, (id_ukolu,))
+        spojeni.commit()
+        kurzor.close()
+        spojeni.close()
+        return True
+    return False
+
+def seznam_aktivnich_ukolu():
+    return zobrazit_ukoly()
+
+def zpracuj_pridani(nazev, popis):
+    return pridat_ukol(nazev, popis)
+
+def zpracuj_aktualizaci(id_ukolu, novy_stav):
+    return aktualizovat_ukol(id_ukolu, novy_stav)
+
+def zpracuj_odstraneni(id_ukolu):
+    return odstranit_ukol(id_ukolu)
+
+def vyhodnot_volbu(volba, vstupy):
+    if volba == "1":
+        nazev, popis = vstupy
+        return zpracuj_pridani(nazev, popis)
+
+    elif volba == "2":
+        return seznam_aktivnich_ukolu()
+
+    elif volba == "3":
+        id_ukolu, novy_stav = vstupy
+        return zpracuj_aktualizaci(id_ukolu, novy_stav)
+
+    elif volba == "4":
+        id_ukolu = vstupy[0]
+        return zpracuj_odstraneni(id_ukolu)
+
+    elif volba == "5":
+        return "konec"
+
+    return "neplatna"
+
+if __name__ == "__main__":
     vytvoreni_tabulky()
     while True:
         print("\n--- Správce úkolů ---")
@@ -42,94 +145,55 @@ def hlavni_menu():
         print("5. Konec")
 
         volba = input("Zadej volbu (1-5): ")
+
         if volba == "1":
-            pridat_ukol()
+            nazev = input("Zadej název úkolu: ")
+            popis = input("Zadej popis úkolu: ")
+            vysledek = vyhodnot_volbu(volba, (nazev, popis))
+            print("Úkol přidán." if vysledek else "Chyba při přidání.")
+
         elif volba == "2":
-            zobrazit_ukoly()
+            ukoly = vyhodnot_volbu(volba, ())
+            if ukoly:
+                for u in ukoly:
+                    print(f"ID: {u['id']}, Název: {u['nazev']}, Popis: {u['popis']}, Stav: {u['stav']}")
+            else:
+                print("Žádné aktivní úkoly.")
+
         elif volba == "3":
-            aktualizovat_ukol()
+            try:
+                id_ukolu = int(input("Zadej ID úkolu pro aktualizaci: "))
+                if not existuje_aktivni_ukol(id_ukolu):
+                    print("Zadané ID nebylo nalezeno mezi aktivními úkoly.")
+                    continue
+
+                novy_stav = input("Nový stav (probíhá/hotovo): ")
+                vysledek = vyhodnot_volbu(volba, (id_ukolu, novy_stav))
+                if vysledek == "neexistuje":
+                    print("Zadané ID nebylo nalezeno mezi aktivními úkoly.")
+                elif vysledek == "neplatny_stav":
+                    print("Zadaný stav není platný.")
+                elif vysledek:
+                    print("Stav aktualizován.")
+                else:
+                    print("Aktualizace selhala.")
+            except ValueError:
+                print("Neplatné ID. Zadej číslo.")
+
         elif volba == "4":
-            odstranit_ukol()
+            try:
+                id_ukolu = int(input("Zadej ID úkolu ke smazání: "))
+                vysledek = vyhodnot_volbu(volba, (id_ukolu,))
+                if vysledek:
+                    print("Úkol smazán.")
+                else:
+                    print("Zadané ID nebylo nalezeno mezi aktivními úkoly.")
+            except ValueError:
+                print("Neplatné ID. Zadej číslo.")
+
         elif volba == "5":
             print("Program ukončen.")
             break
+
         else:
             print("Neplatná volba.")
-
-def pridat_ukol():
-    nazev = input("Zadej název úkolu: ")
-    popis = input("Zadej popis úkolu: ")
-
-    if not nazev or not popis:
-        print("Název ani popis nesmí být prázdné!")
-        return
-
-    spojeni = pripojeni_db()
-    if spojeni:
-        kurzor = spojeni.cursor()
-        dotaz = "INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)"
-        kurzor.execute(dotaz, (nazev, popis))
-        spojeni.commit()
-        print("Úkol byl úspěšně přidán.")
-        kurzor.close()
-        spojeni.close() 
-
-def zobrazit_ukoly():
-    spojeni = pripojeni_db()
-    if spojeni:
-        kurzor = spojeni.cursor()
-        dotaz = "SELECT id, nazev, popis, stav FROM ukoly WHERE stav IN ('nezahájeno', 'probíhá')"
-        kurzor.execute(dotaz)
-        vysledky = kurzor.fetchall()
-
-        if vysledky:
-            print("\nÚkoly:")
-            for radek in vysledky:
-                print(f"ID: {radek[0]}, Název: {radek[1]}, Popis: {radek[2]}, Stav: {radek[3]}")
-        else:
-            print("Žádné aktivní úkoly nebyly nalezeny.")
-
-        kurzor.close()
-        spojeni.close()
-
-def aktualizovat_ukol():
-    zobrazit_ukoly()
-    id_ukolu = input("Zadej ID úkolu, který chceš aktualizovat: ")
-    novy_stav = input("Zadej nový stav (probíhá/hotovo): ")
-
-    if novy_stav not in ['probíhá', 'hotovo']:
-        print("Neplatný stav.")
-        return
-
-    spojeni = pripojeni_db()
-    if spojeni:
-        kurzor = spojeni.cursor()
-        dotaz = "UPDATE ukoly SET stav = %s WHERE id = %s"
-        kurzor.execute(dotaz, (novy_stav, id_ukolu))
-        if kurzor.rowcount == 0:
-            print("Úkol s tímto ID neexistuje.")
-        else:
-            spojeni.commit()
-            print("Stav úkolu byl aktualizován.")
-        kurzor.close()
-        spojeni.close()
-
-def odstranit_ukol():
-    zobrazit_ukoly()
-    id_ukolu = input("Zadej ID úkolu ke smazání: ")
-
-    spojeni = pripojeni_db()
-    if spojeni:
-        kurzor = spojeni.cursor()
-        dotaz = "DELETE FROM ukoly WHERE id = %s"
-        kurzor.execute(dotaz, (id_ukolu,))
-        if kurzor.rowcount == 0:
-            print("Úkol s tímto ID neexistuje.")
-        else:
-            spojeni.commit()
-            print("Úkol byl odstraněn.")
-        kurzor.close()
-        spojeni.close()
-
-if __name__ == "__main__":
-    hlavni_menu()
--- task_manager_test.py
+++ task_manager_test.py
@@ -2,128 +2,91 @@ import mysql.connector
 import pytest
 from datetime import datetime
 
-def pripojeni_test_db():
-    return mysql.connector.connect(
+@pytest.fixture(scope="module")
+def test_db():
+    spojeni = mysql.connector.connect(
         host="localhost",
         user="root",
         password="K4d1budk487!",
         database="task_manager_test"
     )
-
-def smaz_testovaci_ukol(nazev):
-    spojeni = pripojeni_test_db()
     kurzor = spojeni.cursor()
-    kurzor.execute("DELETE FROM ukoly WHERE nazev = %s", (nazev,))
+    kurzor.execute("""
+        CREATE TABLE IF NOT EXISTS ukoly (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nazev VARCHAR(100) NOT NULL,
+            popis TEXT NOT NULL,
+            stav ENUM('nezahájeno', 'probíhá', 'hotovo') DEFAULT 'nezahájeno',
+            datum_vytvoreni DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+    spojeni.commit()
+    yield spojeni
+    kurzor.execute("DELETE FROM ukoly")
     spojeni.commit()
     kurzor.close()
     spojeni.close()
 
-def test_pridani_ukolu_pozitivni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
+@pytest.fixture
+def vloz_ukol(test_db):
+    def _vloz(nazev, popis):
+        kurzor = test_db.cursor()
+        kurzor.execute("INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)", (nazev, popis))
+        test_db.commit()
+        kurzor.execute("SELECT id FROM ukoly WHERE nazev = %s", (nazev,))
+        return kurzor.fetchone()[0]
+    return _vloz
 
+def test_pridani_ukolu_pozitivni(test_db):
     nazev = "Testovací úkol"
-    popis = "Testovací popis úkolu."
+    popis = "Testovací popis"
+    kurzor = test_db.cursor(dictionary=True)
 
-    kurzor.execute(
-        "INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)",
-        (nazev, popis)
-    )
-    spojeni.commit()
+    kurzor.execute("INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)", (nazev, popis))
+    test_db.commit()
 
     kurzor.execute("SELECT * FROM ukoly WHERE nazev = %s", (nazev,))
     vysledek = kurzor.fetchone()
-
     assert vysledek is not None
-    assert vysledek[1] == nazev
-    assert vysledek[2] == popis
+    assert vysledek["nazev"] == nazev
+    assert vysledek["popis"] == popis
 
-    kurzor.close()
-    spojeni.close()
-    smaz_testovaci_ukol(nazev)
-
-def test_pridani_ukolu_negativni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
-
-    nazev = "TEST" * 999
-    popis = "Test s překročením počtu povolených znaků."
-
+def test_pridani_ukolu_negativni(test_db):
+    nazev = "X" * 999
+    popis = "Příliš dlouhý název"
+    kurzor = test_db.cursor()
     with pytest.raises(mysql.connector.Error):
         kurzor.execute("INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)", (nazev, popis))
-        spojeni.commit()
+        test_db.commit()
 
-    kurzor.close()
-    spojeni.close()
-
-def test_aktualizace_ukolu_pozitivni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
-
-    nazev = "Aktualizace úkolu"
-    popis = "Před aktualizací"
-
-    kurzor.execute("INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)", (nazev, popis))
-    spojeni.commit()
-
-    kurzor.execute("SELECT id FROM ukoly WHERE nazev = %s", (nazev,))
-    ukol_id = kurzor.fetchone()[0]
-
+def test_aktualizace_ukolu_pozitivni(test_db, vloz_ukol):
+    ukol_id = vloz_ukol("Aktualizace", "Test")
+    kurzor = test_db.cursor()
     kurzor.execute("UPDATE ukoly SET stav = %s WHERE id = %s", ("hotovo", ukol_id))
-    spojeni.commit()
+    test_db.commit()
 
     kurzor.execute("SELECT stav FROM ukoly WHERE id = %s", (ukol_id,))
     stav = kurzor.fetchone()[0]
     assert stav == "hotovo"
 
-    kurzor.close()
-    spojeni.close()
-    smaz_testovaci_ukol(nazev)
-
-def test_aktualizace_ukolu_negativni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
-
-    neexistujici_id = 999999
-    kurzor.execute("UPDATE ukoly SET stav = %s WHERE id = %s", ("hotovo", neexistujici_id))
-    spojeni.commit()
-
+def test_aktualizace_ukolu_negativni(test_db):
+    kurzor = test_db.cursor()
+    kurzor.execute("UPDATE ukoly SET stav = %s WHERE id = %s", ("hotovo", 123456))
+    test_db.commit()
     assert kurzor.rowcount == 0
 
-    kurzor.close()
-    spojeni.close()
-
-def test_odstraneni_ukolu_pozitivni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
-
-    nazev = "Úkol ke smazání"
-    popis = "Testovací úkol pro smazání."
-
-    kurzor.execute("INSERT INTO ukoly (nazev, popis) VALUES (%s, %s)", (nazev, popis))
-    spojeni.commit()
-
-    kurzor.execute("SELECT id FROM ukoly WHERE nazev = %s", (nazev,))
-    ukol_id = kurzor.fetchone()[0]
-
+def test_odstraneni_ukolu_pozitivni(test_db, vloz_ukol):
+    ukol_id = vloz_ukol("Smazat", "Test")
+    kurzor = test_db.cursor()
     kurzor.execute("DELETE FROM ukoly WHERE id = %s", (ukol_id,))
-    spojeni.commit()
+    test_db.commit()
 
     kurzor.execute("SELECT * FROM ukoly WHERE id = %s", (ukol_id,))
-    assert kurzor.fetchone() is None
+    vysledek = kurzor.fetchone()
+    assert vysledek is None
 
-    kurzor.close()
-    spojeni.close()
-
-def test_odstraneni_ukolu_negativni():
-    spojeni = pripojeni_test_db()
-    kurzor = spojeni.cursor()
-
-    neexistujici_id = 999999
-    kurzor.execute("DELETE FROM ukoly WHERE id = %s", (neexistujici_id,))
-    spojeni.commit()
-
+def test_odstraneni_ukolu_negativni(test_db):
+    kurzor = test_db.cursor()
+    kurzor.execute("DELETE FROM ukoly WHERE id = %s", (123456,))
+    test_db.commit()
     assert kurzor.rowcount == 0
-
-    kurzor.close()
-    spojeni.close()
